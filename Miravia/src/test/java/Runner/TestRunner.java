c88992f9e5e70a57dce285aea93689af4abeb21c
Zakaria <z.mahlaoui1@gmail.com>
2024-07-26 10:46:34 +0200

    update

--- Miravia/src/test/java/Runner/TestRunner.java
+++ Miravia/src/test/java/Runner/TestRunner.java
@@ -7,7 +7,7 @@ import Pages.BasePage;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources", 
+@CucumberOptions(features = "SeleniumJenkinsGradle/Miravia/src/test/resources", 
                  glue = "Steps", 
                  plugin = { "pretty", "html:target/cucumber-reports" }
                  //tags = "@Courses"
